db1d90c1bc63f8eb73f2cf036c57985699258aa5
cadelaa <mitchzeller@hotmail.com>
2025-04-19 21:08:44 +1000

    Added retro typeface, created main menu & added play button fuctionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public void GoToScene (string scenename)
+    {
+        SceneManager.LoadScene(scenename);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
+        Debug.Log("Application has quit.");
+    }
+}
